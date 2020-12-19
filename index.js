f03fa03af3b1898d61d025f91041f5e88f6362af
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-19 21:18:23 +0100

    new redirects

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function bruh() {
-	window.location.href = "./bruh/index.html";
+function redir(t) {
+	switch (t) {
+		case "bruh":
+			window.location.href = "./bruh/index.html";
+			break;
+		case "karesz":
+			window.location.href = "./karesz/index.md";
+			break;
+
+		default:
+			console.error("invalid redirect");
+			alert("invalid redirect");
+			break;
+	}
 }
