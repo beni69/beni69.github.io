8a754dff54dc97658a50730a835ae040eb8d1ede
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-20 11:27:27 +0100

    bruh momento numeros tres

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function redir(t) {
 	switch (t) {
 		case "bruh":
-			window.location.href = "./bruh/";
+			window.location.href = "./bruh/index.html";
 			break;
 		case "karesz":
 			window.location.href = "./karesz/index.html";
