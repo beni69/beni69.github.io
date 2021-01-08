c0430b86bbb72d80d0c7a2adb3564d3112633072
beni69 <73666440+beni69@users.noreply.github.com>
2021-01-08 21:06:50 +0100

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function redir(t) {
-	switch (t) {
-		case "bruh":
-			window.location.href = "./bruh/";
-			break;
-		case "karesz":
-			window.location.href = "./karesz/";
-			break;
+    switch (t) {
+        case "bruh":
+            window.location.href = "./bruh/";
+            break;
+        case "karesz":
+            window.location.href = "./karesz/";
+            break;
 
-		default:
-			console.error("invalid redirect");
-			alert("invalid redirect");
-			break;
-	}
+        default:
+            console.error("invalid redirect");
+            alert("invalid redirect");
+            break;
+    }
 }
