1e164a7ce7a2c986cd37a34aa7c55d01122ff08a
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-09 22:06:07 +0100

    ms

--- reactiontime/stats.js
+++ reactiontime/stats.js
@@ -1,7 +1,8 @@
 fetch("http://api.karesz.xyz/karesz/reaction", {method: "GET"})
     .then(response => response.json())
     .then(data => {
-        document.getElementById("average").innerHTML = Math.round(data.average);
-        document.getElementById("min").innerHTML = data.min;
+        document.getElementById("average").innerHTML =
+            Math.round(data.average) + "ms";
+        document.getElementById("min").innerHTML = data.min + "ms";
     })
     .catch(err => console.error(err));
