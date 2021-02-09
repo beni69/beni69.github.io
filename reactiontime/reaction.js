d9b31c2d31d87b858d8a6d9e3a8f397a29951a61
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-09 21:18:05 +0100

    new reaction time database

--- reactiontime/reaction.js
+++ reactiontime/reaction.js
@@ -26,6 +26,7 @@ const game = () => {
             h1.innerHTML = `${time}ms`;
             body.style.backgroundColor = "lightblue";
             audio.pause();
+            sendToDb(time);
 
             html.onclick = idle;
         };
@@ -40,5 +41,18 @@ const idle = () => {
     body.style.backgroundColor = "white";
     html.onclick = game;
 };
+
+async function sendToDb(time) {
+    fetch("http://api.karesz.xyz/karesz/reaction", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({time: time}),
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+}
 
 html.onclick = game;
