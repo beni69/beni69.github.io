60addd58bccfedffe9347d4eb6115fa8f1eb606b
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-10 11:04:52 +0100

    global and local stats

--- reactiontime/stats.js
+++ reactiontime/stats.js
@@ -1,8 +1,27 @@
-fetch("http://api.karesz.xyz/karesz/reaction", {method: "GET"})
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("average").innerHTML =
-            Math.round(data.average) + "ms";
-        document.getElementById("min").innerHTML = data.min + "ms";
-    })
-    .catch(err => console.error(err));
+// global stats
+function LoadGlobal() {
+    fetch("http://api.karesz.xyz/karesz/reaction", {method: "GET"})
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("avgG").innerHTML =
+                Math.round(data.average) + "ms";
+            document.getElementById("minG").innerHTML = data.min + "ms";
+        })
+        .catch(err => console.error(err));
+}
+
+// local stats
+function LoadLocal() {
+    const average = localStorage.getItem("average");
+    document.getElementById("avgL").innerHTML =
+        average == null ? "No data" : Math.round(average);
+    const min = localStorage.getItem("min");
+    document.getElementById("minL").innerHTML =
+        min == null ? "No data" : Math.round(min);
+}
+
+function ResetLocal() {
+    localStorage.clear();
+    alert("Done!");
+    location.reload();
+}
