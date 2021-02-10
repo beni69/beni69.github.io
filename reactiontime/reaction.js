8b8e34b958690ded255192b5f492843a0108c4e8
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-10 10:56:53 +0100

    anti cheat

--- reactiontime/reaction.js
+++ reactiontime/reaction.js
@@ -32,6 +32,49 @@ const game = () => {
         };
     }, wait);
     return;
+
+    async function sendToDb(time) {
+        // global stats
+        fetch("http://api.karesz.xyz/karesz/reaction", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({time: time}),
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
+
+        // local stats
+        let results = JSON.parse(localStorage.getItem("results"));
+        if (results == null) results = [];
+        results.push(time);
+        localStorage.setItem("results", JSON.stringify(results));
+        localStorage.setItem("average", average(results));
+        localStorage.setItem("max", max(results));
+        localStorage.setItem("min", min(results));
+
+        function average(array) {
+            let sum = 0;
+            array.forEach(item => (sum += item));
+            return sum / array.length;
+        }
+        function max(array) {
+            let best = -1;
+            array.forEach(item => {
+                if (item > best) best = item;
+            });
+            return best;
+        }
+        function min(array) {
+            let best = Number.MAX_VALUE;
+            array.forEach(item => {
+                if (item < best) best = item;
+            });
+            return best;
+        }
+    }
 };
 
 const idle = () => {
@@ -41,18 +84,5 @@ const idle = () => {
     body.style.backgroundColor = "white";
     html.onclick = game;
 };
-
-async function sendToDb(time) {
-    fetch("http://api.karesz.xyz/karesz/reaction", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({time: time}),
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
-}
 
 html.onclick = game;
