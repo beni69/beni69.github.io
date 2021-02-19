2d8acc08e964ee11e4a9031c8dcafe96f09b5890
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-19 15:01:44 +0100

    new update

--- shortener/shortener.js
+++ shortener/shortener.js
@@ -1,14 +1,40 @@
-async function submit() {
-    const dest = document.getElementById("dest").value;
+document.addEventListener("DOMContentLoaded", event => {
+    document.getElementById("form").addEventListener("submit", async e => {
+        e.preventDefault();
 
-    const res = await fetch("http://api.karesz.xyz/shortener", {
-        method: "POST",
-        headers: {"Content-type": "application/json"},
-        body: JSON.stringify({dest}),
+        const input = document.getElementById("dest").value;
+        const p = document.getElementById("p");
+        const a = document.getElementById("a");
+
+        p.innerText = "";
+        a.innerText = "";
+
+        const r = /^http(s|):\/\/(krsz\.me|u\.karesz\.xyz)($|\/.+$)/i;
+
+        if (r.test(input)) {
+            // get stats of a link
+            const res = await fetch(
+                `http://api.karesz.xyz/shortener?code=${input}`
+            );
+            const d = await res.json();
+            console.log(d);
+            p.innerText = `
+            Created at: ${new Date(d.timestamp).toLocaleString()}
+            Clicks: ${d.clicks}
+            Link: ${d.url}
+            Destination: ${d.dest}
+            `.trim();
+        } else {
+            // create a link
+            // const res = await fetch("http://api.karesz.xyz/shortener", {
+            //     method: "POST",
+            //     headers: { "Content-type": "application/json" },
+            //     body: JSON.stringify({ dest: input }),
+            // });
+            // const body = await res.json();
+            // const url = body.created.url;
+            // a.innerText = url;
+            // a.href = url;
+        }
     });
-    const body = await res.json();
-    const url = body.created.url;
-    const output = document.getElementById("res");
-    output.innerText = url;
-    output.href = url;
-}
+});
